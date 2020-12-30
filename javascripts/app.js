919f5c5eb91a9932437d8f20fffd680631b5f52e
Joe <modulusman@gmail.com>
2020-12-30 12:10:40 -0800

    Demo Site | Updates testimonials slider at 1024 and above to include multiple cards on each carousel slide.

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,20 +1,29 @@
-new Splide('.splide').mount();
-
-const learnMoreLinks  = document.querySelectorAll('.learn-more-button');
-const hamburgerMenu   = document.querySelector('.mobile-menu-toggle');
-const menuDrawer = document.querySelector('.mobile-menu-drawer');
-
-if(learnMoreLinks) {
-  learnMoreLinks.forEach((link) => {
-    link.setAttribute('href', 'https://clickup.com/');
-  });
-}
-
-if(hamburgerMenu) {
-  hamburgerMenu.addEventListener('click', () => {
-    hamburgerMenu.classList.toggle('menu-opened');
-    if(menuDrawer) {
-      menuDrawer.classList.toggle('drawer-opened');
+document.addEventListener('DOMContentLoaded', function () {
+  new Splide('.splide', {
+    perPage: 2,
+    breakpoints: {
+      1023: {
+        perPage: 1,
+      }
     }
-  });
-}
+  }).mount();
+
+  const learnMoreLinks = document.querySelectorAll('.learn-more-button');
+  const hamburgerMenu = document.querySelector('.mobile-menu-toggle');
+  const menuDrawer = document.querySelector('.mobile-menu-drawer');
+
+  if (learnMoreLinks) {
+    learnMoreLinks.forEach((link) => {
+      link.setAttribute('href', 'https://clickup.com/');
+    });
+  }
+
+  if (hamburgerMenu) {
+    hamburgerMenu.addEventListener('click', () => {
+      hamburgerMenu.classList.toggle('menu-opened');
+      if (menuDrawer) {
+        menuDrawer.classList.toggle('drawer-opened');
+      }
+    });
+  }
+});
