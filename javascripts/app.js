47c65acb87c7862141fae7b4adfa85008278294b
Joe <modulusman@gmail.com>
2020-12-29 22:49:01 -0800

    Demo Site | Adds clic event to hamburger menu.
    Also hides hamburger menu at 1024 and above, where the desktop
    nav will show instead.
    
    Sets styles for hamburger menu on click.

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,8 +1,16 @@
 new Splide('.splide').mount();
 
-const learnMoreLinks = document.querySelectorAll('.learn-more-button');
+const learnMoreLinks  = document.querySelectorAll('.learn-more-button');
+const hamburgerMenu   = document.querySelector('.mobile-menu-toggle');
+
 if(learnMoreLinks) {
   learnMoreLinks.forEach((link) => {
     link.setAttribute('href', 'https://clickup.com/');
+  });
+}
+
+if(hamburgerMenu) {
+  hamburgerMenu.addEventListener('click', () => {
+    hamburgerMenu.classList.toggle('menu-opened');
   });
 }
