3336f64ce660a980b5eee9e70a16262eb0e44db6
Joe <modulusman@gmail.com>
2020-12-29 20:46:41 -0800

    Demo Site | Adds javascript to update all hrefs of Learn more CTA buttons on the site.
    These buttons will now point to the CLickUp site and open in a new
    tab.

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,1 +1,8 @@
 new Splide('.splide').mount();
+
+const learnMoreLinks = document.querySelectorAll('.learn-more-button');
+if(learnMoreLinks) {
+  learnMoreLinks.forEach((link) => {
+    link.setAttribute('href', 'https://clickup.com/');
+  });
+}
